9e6d5187747c19286ce6b9aeff8b30f46121ed91
Andrew Starikov <andyst@mail.ru>
2021-05-06 17:14:11 +0500

    test dataset update

--- ml_project/tests/data/test_dataset.py
+++ ml_project/tests/data/test_dataset.py
@@ -50,12 +50,18 @@ def test_split_train_val_data():
     splitting_params = {'val_size': val_size, 'random_state': random_state}
     splitting_data = SplittingParams(**splitting_params)
     df_train, df_val = split_train_val_data(data, splitting_data)
-    assert len(df_train) - 1\
+
+    len_train = len(df_train)
+    len_val = len(df_val)
+
+    assert len_train - 1\
            <= int(FAKE_DATASET_SIZE * (1 - val_size))\
-           <= len(df_train) + 1
-    assert len(df_val) - 1\
+           <= len_train + 1
+    assert len_val - 1\
            <= int(FAKE_DATASET_SIZE * val_size)\
-           <= len(df_val) + 1
+           <= len_val + 1
+    assert len_train + len_val == len(data)
+
     columns_count = len(CATEGORICAL_FEATURES) + len(NUMERICAL_FEATURES) + 1
     assert df_train.shape[1] == columns_count
     assert df_val.shape[1] == columns_count
