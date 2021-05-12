00d831f714d5f782839255c4b3f7e1f07be6af3b
Andrew Starikov <andyst@mail.ru>
2021-05-12 16:13:37 +0500

    add Github actions

---
+++ online_inference/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+
+def parse_requirements(path):
+    with open(path) as fin:
+        requirements = []
+        for line in fin:
+            requirements.append(line.strip())
+    return requirements
+
+
+setup(
+    name="homework2",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Homework2 on ML in production",
+    author="Andrew Starikov",
+    install_requires=parse_requirements("./requirements.txt"),
+    license="MIT",
+)
